d696c020eb300e4d1d7a9bb1add586774a6f057f
SANDRA HERNANDEZ VALERO <sandy_hv5@hotmail.com>
2023-03-08 12:25:34 -0600

    agregando personajes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,38 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 //Crear un componente
 //Definir props 
 //Utilizar props
+const amiiboUrl = 'https://www.amiiboapi.com/api/amiibo/';
 
-const Panel = ({isOpen,title,handleClick, children}) => {
-  
+const Panel = ({isOpen,title,handleClick, children, image,id,name,type, amiiboSeries,character}) => {
   return (
-    <div  >
-      <div  style={{color:isOpen? 'green' :'red'}} onClick={handleClick} >
-      <p>{title}</p>
+    <div >
+      
+      <button  style={{color:isOpen? '#229954' :'#A93226', border:isOpen?  '4px solid #5499C7':'3px solid #4A235A', width: 300, height: '100%', margin: 10 }} onClick={handleClick} >
+
+      <div style={{display:'flex', flexDirection:'row',alignItems:'center'}}>
+      <img src={image} alt="personaje" style={{width: 50, backgroundColor: '#BDC3C7', borderRadius:50, padding: 10 ,margin: 5}} />
+      <h3>¿Quién es tu personaje favorito?</h3>
+      <p style={{transform:isOpen? 'rotate(180)': 'none'}}>+</p>
+
       </div>
-      {isOpen && <p>{children}</p>}
+      </button>
+     
+      {isOpen && <div style={{color:isOpen? '#229954' :'#A93226'}}>
+        <h3>Nombre: {name}</h3>
+        <p>Tipo: {type}</p>
+        <p>Amibibo: {amiiboSeries}</p>
+        <p>Character: {character}</p>
+        </div>}
+      
+      
+     
     </div>
   )
 }
-
+//// BORRAR
 const data = [{
   id:1,
   question: 'pregunta 1',
@@ -38,19 +54,39 @@ const data = [{
   question: 'pregunta 5',
   answer: 'answer 5'
 }]
+//// BORRAR
+
 
 const Accordeon = () => {
   const [isOpen, setIsOpen] = useState(null)
+
+  const [chars, setChars] = useState([]);
+
+  useEffect(()=>{
+    const getChars = async () => {
+       const response = await fetch(amiiboUrl);
+       const data     = await response.json();
+       setChars(data.amiibo)
+       console.log(data.amiibo);
+    }
+    getChars();
+  },[]);
    
-  const handleClick = (id) => {
-    setIsOpen(id)
+  const handleClick = (tail) => {
+    setIsOpen(tail)
   }
 
   return (
-    <div  >
+    <div>
       
-      {data.map((item)=>(
-      <Panel key={item.id} id={item.id} isOpen={item.id===isOpen} handleClick={()=>handleClick(item.id)} title={item.question}>
+      {chars.map((item)=>(
+      <Panel key={item.tail} id={item.tail} isOpen={item.tail===isOpen} handleClick={()=>handleClick(item.tail)} title={item.question}
+      image={item.image}
+      name={item.name}
+      type={item.type}
+      amiiboSeries={item.amiiboSeries}
+      character={item.character}
+      >
       {item.answer}
      </Panel>))}
      
@@ -61,9 +97,8 @@ const Accordeon = () => {
   )
 }
 
+//  APP //
 function App() {
-
-
   return (
    
     <div>
