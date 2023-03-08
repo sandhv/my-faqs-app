dd2b1ed319cb2e6489a3a4ff5b479031b2d66dcb
SANDRA HERNANDEZ VALERO <sandy_hv5@hotmail.com>
2023-03-07 22:11:35 -0600

    ejercicio faqs

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,74 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+//Crear un componente
+//Definir props 
+//Utilizar props
+
+const Panel = ({isOpen,title,handleClick, children}) => {
+  
+  return (
+    <div  >
+      <div  style={{color:isOpen? 'green' :'red'}} onClick={handleClick} >
+      <p>{title}</p>
+      </div>
+      {isOpen && <p>{children}</p>}
+    </div>
+  )
+}
+
+const data = [{
+  id:1,
+  question: 'pregunta 1',
+  answer: 'answer 1'
+}, {
+  id:2,
+  question: 'pregunta 2',
+  answer: 'answer 2'
+}, {
+  id:3,
+  question: 'pregunta 3',
+  answer: 'answer 3'
+}, {
+  id:4,
+  question: 'pregunta 4',
+  answer: 'answer 4'
+}, {
+  id:5,
+  question: 'pregunta 5',
+  answer: 'answer 5'
+}]
+
+const Accordeon = () => {
+  const [isOpen, setIsOpen] = useState(null)
+   
+  const handleClick = (id) => {
+    setIsOpen(id)
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div  >
+      
+      {data.map((item)=>(
+      <Panel key={item.id} id={item.id} isOpen={item.id===isOpen} handleClick={()=>handleClick(item.id)} title={item.question}>
+      {item.answer}
+     </Panel>))}
+     
+
+     
+     
+    </div>
+  )
+}
+
+function App() {
+
+
+  return (
+   
+    <div>
+     <h1>FAQs ❔</h1>
+     <Accordeon/>
     </div>
   )
 }
